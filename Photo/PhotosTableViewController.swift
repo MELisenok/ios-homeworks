ff77c522588288946ad2c7269a3d961375deae86
MELisenok <787mel@gmail.com>
2023-04-20 12:41:41 -0400

    HW_6 UICollectionView

---
+++ Photo/PhotosTableViewController.swift
@@ -0,0 +1,127 @@
+//
+//  PhotosTableViewController.swift
+//  Navigation
+//
+//  Created by Лена on 4/20/23.
+//
+
+import UIKit
+
+final class PhotosTableViewController: UIViewController {
+    
+    weak var delegate: PhotosTableDelegate?
+    
+    private let photos: [Photos] = Photos.selectPhoto()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Photos"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = UIColor.black
+        return label
+    }()
+    
+    private lazy var galleryButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "arrow.forward"), for: .normal)
+        button.addTarget(self, action: #selector(galleryButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var collectionView = makeCollectionView(scrollDirection: .horizontal)
+    
+    @objc private func galleryButtonPressed() {
+        delegate?.galleryButtonPressed()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        setupCollectionView()
+    }
+    
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    
+    func layout() {
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        view.addSubview(collectionView)
+        view.addSubview(galleryButton)
+        
+        NSLayoutConstraint.activate([
+            galleryButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            galleryButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: galleryButton.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
+
+
+extension PhotosTableViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotosTableViewCell.identifier, for: indexPath) as! PhotosTableViewCell
+        cell.setupCell(photo: photos[indexPath.row])
+        return cell
+    }
+}
+
+extension PhotosTableViewController: UICollectionViewDelegateFlowLayout {
+    private var sideInset: CGFloat { return 8 }
+    private var elementCount: CGFloat { return 3 }
+    private var insetsCount: CGFloat { return elementCount + 1 }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = collectionView.bounds.height - sideInset * 2
+        return CGSize(width: height, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return sideInset
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: sideInset, left: sideInset, bottom: sideInset, right: sideInset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        sideInset
+    }
+}
+
+extension UIViewController {
+    func makeCollectionView(scrollDirection: UICollectionView.ScrollDirection) -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = scrollDirection
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(PhotosTableViewCell.self, forCellWithReuseIdentifier: PhotosTableViewCell.identifier)
+        return collectionView
+    }
+}
+
+protocol PhotosTableDelegate: AnyObject {
+    func galleryButtonPressed()
+    
+}
---
+++ Photo/PhotosViewController.swift
@@ -0,0 +1,109 @@
+//
+//  PhotosViewController.swift
+//  Navigation
+//
+//  Created by Лена on 4/20/23.
+//
+
+import UIKit
+
+final class PhotosViewController: UIViewController {
+    
+    private let photos: [Photos] = Photos.selectPhoto()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout) //.zero тк размеры выставим в констрейнтах
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemGray2
+        collectionView.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: PhotosCollectionViewCell.identifier) //регистрируем ячейку
+        return collectionView
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        title = "Photos"
+        layout()
+        setupCollectionView()
+        
+    }
+    
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    private func layout() {
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+            
+        ])
+    }
+    
+}
+
+
+extension PhotosViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotosCollectionViewCell.identifier, for: indexPath) as! PhotosCollectionViewCell
+        cell.setupCell(photo: photos[indexPath.row]) //если бы в PhotosCollectionViewCell использовали вариант с инициализатором, то эту строчку бы не надо было писать!
+        return cell
+    }
+}
+
+
+extension PhotosViewController: UICollectionViewDelegateFlowLayout {
+    
+    private var sideInset: CGFloat {return 8} //размер отступом между ячейками (в extension можно только хранимые переменные!)
+    private var elementCount: CGFloat {return 3}
+    private var insetsCount: CGFloat {return elementCount + 1} //количество отступов
+    
+    //размеры ячейки:
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - sideInset) / 3 - sideInset //collectionView.bounds.width - узнаем ширину экрана
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return sideInset
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: sideInset, left: sideInset, bottom: sideInset, right: sideInset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        sideInset
+    }
+    
+}
